17b5387a154efd5cdc20cfd99d39f1095578e29a
Siddharth <officialsiddharthbisht@gmail.com>
2023-10-12 09:33:26 +0530

    feat: feedback UIs

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,25 @@
 <script>
-	export let name;
+	import FeedbackList from "./components/FeedbackList.svelte";
+
+	let feedback = [
+		{
+		id:1,
+		rating: 10,
+		text: "some random text here",
+		},
+		{
+		id:2,
+		rating: 9,
+		text: "some random text here",
+		},
+		{
+		id:3,
+		rating: 10,
+		text: "some random text here",
+		},
+	]
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<main class="container">
+	<FeedbackList {feedback} />
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
