5b076f4208d9a31134b9681d6d3d58f4c12399d8
Siddharth <officialsiddharthbisht@gmail.com>
2023-10-12 10:08:00 +0530

    feat: total feedbacks and average rating

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
 	import FeedbackList from "./components/FeedbackList.svelte";
-
+	import FeedbackStat from "./components/FeedbackStat.svelte";
 	let feedback = [
 		{
 		id:1,
@@ -18,8 +18,16 @@
 		text: "some random text here",
 		},
 	]
+	$: count = feedback.length
+	$: average = (feedback.reduce((a,{rating})=>a+rating,0)/feedback.length).toFixed(2)
+
+	const delteFeedback = (e) =>{
+	const itemId =  e.detail;
+	feedback = feedback.filter((item)=> item.id !== itemId)
+	}
 </script>
 
 <main class="container">
-	<FeedbackList {feedback} />
+	<FeedbackStat {count} {average} />
+	<FeedbackList {feedback} on:delete-feedback={delteFeedback}/>
 </main>
